71abec0fe84bb105200a8f00925d92dfae5c1b2a
Akarsh Seggemu <akarsh@tenjin.com>
2022-06-03 18:36:30 +0200

    [TENJIN-12196] Update Tenjin Unity SDK to 1.12.18
    - Update SampleScene iOS with ATT prompt code

--- Assets/Scenes/SampleScene.cs
+++ Assets/Scenes/SampleScene.cs
@@ -1,6 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_IOS
+using UnityEngine.iOS;
+#endif
 
 public class SampleScene : MonoBehaviour
 {
@@ -28,25 +32,46 @@ public class SampleScene : MonoBehaviour
     public void TenjinConnect()
     {
         BaseTenjin instance = Tenjin.getInstance("WZVBMOQEHIMJZD2CZ1VQYIGR16PQAWWC");
-
-        instance.SetAppStoreType(AppStoreType.googleplay);
-
 #if UNITY_IOS
-      if (new Version(Device.systemVersion).CompareTo(new Version("14.0")) >= 0) {
-        // Tenjin wrapper for requestTrackingAuthorization
-        instance.RequestTrackingAuthorizationWithCompletionHandler((status) => {
-          Debug.Log("===> App Tracking Transparency Authorization Status: " + status);
-
-          // Sends install/open event to Tenjin
-          instance.Connect();
-
+        // instance.RegisterAppForAdNetworkAttribution();
+       float iOSVersion = float.Parse(UnityEngine.iOS.Device.systemVersion);
+       if (iOSVersion == 14.0) {
+            // Tenjin wrapper for requestTrackingAuthorization
+            instance.RequestTrackingAuthorizationWithCompletionHandler((status) => {
+            Debug.Log("===> App Tracking Transparency Authorization Status: " + status);
+            switch (status)
+            {
+                case 0:
+                    Debug.Log("ATTrackingManagerAuthorizationStatusNotDetermined case");
+                    Debug.Log("Not Determined");
+                    Debug.Log("Unknown consent");
+                    break;
+                case 1:
+                    Debug.Log("ATTrackingManagerAuthorizationStatusRestricted case");
+                    Debug.Log(@"Restricted");
+                    Debug.Log(@"Device has an MDM solution applied");
+                    break;
+                case 2: 
+                    Debug.Log("ATTrackingManagerAuthorizationStatusDenied case");
+                    Debug.Log("Denied");
+                    Debug.Log("Denied consent");
+                    break;
+                case 3:
+                    Debug.Log("ATTrackingManagerAuthorizationStatusAuthorized case");
+                    Debug.Log("Authorized");
+                    Debug.Log("Granted consent");
+                    instance.Connect();
+                    break;
+                default:
+                    Debug.Log("Unknown");
+                    break;
+            }
         });
-      }
-      else {
+       } else {
           instance.Connect();
       }
 #elif UNITY_ANDROID
-
+        instance.SetAppStoreType(AppStoreType.googleplay);
         // Sends install/open event to Tenjin
         instance.Connect();
 
